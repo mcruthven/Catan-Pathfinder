da07f9b36c215a250fec1ce315a0604121faba73
Chris <sihrc.c.lee@gmail.com>
2014-12-08 11:09:55 -0500

    ADD: Controller and dijkstra's algo

--- src/main.py
+++ src/main.py
@@ -3,12 +3,75 @@ from views import Display
 from evaluators import EvaluatorA as EV
 from algorithms import dijkstra
 
-def clickedInBounds(point, bounds):
-    return bounds[0] < point.getX() < bounds[2] and bounds[1] < point.getY() < bounds[3]
+class Controller:
+    def __init__(self, display, board, find_path):
+        self.display = display
+        self.board = board
+        self.find_path = find_path
+
+        self.initMembers()
+
+        self.display.drawBoard(self.board)
+
+    def initMembers(self):
+        # Click action
+        self.action = None
+
+        # Start / End vertices / circles
+        self.start_circle = None
+        self.end_circle = None
+        self.start = None
+        self.end = None
+
+        # Whether or not to recalculate the path
+        self.changed = False
+
+    def drawButtons(self, *args):
+        self.buttons = []
+        for arg in args:
+            self.buttons.append(arg)
+            self.display.drawButton(arg[-1], arg[0:2], arg[2:4], scale = True)
+
+    def handleClick(self):
+        clicked = self.display.input()
+        if clicked == None:
+            return
+        # Check if clicked buttons
+        if self.clickedInBounds(clicked, self.buttons[0]):
+            self.action = "start"
+            if self.start_circle != None:
+                self.start_circle.undraw()
+        elif self.clickedInBounds(clicked, self.buttons[1]):
+            self.action = "end"
+            if self.end_circle != None:
+                self.end_circle.undraw()
+        else: # Otherwise, selecting a node
+            if self.action == "start":
+                self.start = self.board.get_vertex_from_position(self.display._convertToNormal((clicked.getX(), clicked.getY())))
+                if self.start != None:
+                    self.start_circle = self.display._drawCircle(self.start.pos, 20, "red")
+                    self.changed = True
+                    self.action = None
+                
+            if self.action == "end":
+                self.end = self.board.get_vertex_from_position(self.display._convertToNormal((clicked.getX(), clicked.getY())))
+                if self.end != None:
+                    self.end_circle = self.display._drawCircle(self.end.pos, 20, "blue")
+                    self.changed = True
+                    self.action = None
+
+        # Check to see if we need to redraw the path
+        if self.changed and self.start and self.end:
+            self.find_path(self.board.vertices.values(), self.start, self.end)
+            changed = False
+
+    def clickedInBounds(self, point, bounds):
+        return bounds[0] < point.getX() < bounds[2] and bounds[1] < point.getY() < bounds[3]
 
 def GameLoop():
-    START_BUTTON = (20, 25, 120, 70)
-    END_BUTTON = (20, 100, 120, 150)
+    # Buttons
+    START_BUTTON = (20, 25, 120, 70, "Starting Node")
+    END_BUTTON = (20, 100, 120, 150, "Ending Node")
 
     # Create the board
     board = Board(3)
@@ -16,52 +79,15 @@ def GameLoop():
 
     # Show the initial board
     display = Display()
-    display.drawBoard(board)
-    # Draw set Starting node button
-    _a, _b = display.drawButton("Starting Node", START_BUTTON[0:2], START_BUTTON[2:4], scale = True)
-    
-    # Draw set Ending node button
-    _c, _d = display.drawButton("Ending Node", END_BUTTON[0:2], END_BUTTON[2:4], scale = True)
-    
-    action = None
-    start_circle = None
-    end_circle = None
-    changed = False
-    start = None
-    end = None
 
+    # Controller Initialization
+    controller = Controller(display, board, dijkstra.find_path)
+    controller.drawButtons(START_BUTTON, END_BUTTON)
+    
     while True:
-        clicked = display.input()
-        if clicked != None:
-            if clickedInBounds(clicked, START_BUTTON):
-                action = "start"
-                if start_circle != None:
-                    start_circle.undraw()
-            elif clickedInBounds(clicked, END_BUTTON):
-                action = "end"
-                if end_circle != None:
-                    end_circle.undraw()
-            else:
-                if action == "start":
-                    start = board.get_vertex_from_position(display._convertToNormal((clicked.getX(), clicked.getY())))
-                    if start != None:
-                        start_circle = display._drawCircle(start.pos, 20, "red")
-                        changed = True
-                        action = None
-                    
-                if action == "end":
-                    end = board.get_vertex_from_position(display._convertToNormal((clicked.getX(), clicked.getY())))
-                    if end != None:
-                        end_circle = display._drawCircle(end.pos, 20, "blue")
-                        changed = end != None
-                        action = None
-
-        if changed and start and end:
-            # dijkstra.find_path(board.vertices.values(), start, end)
-            changed = False
-
+        controller.handleClick()
         display.update()
-    display.wait()
+
     display.close()
 
 def DrawBoardTest():
