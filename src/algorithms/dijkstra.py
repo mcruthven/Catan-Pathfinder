eef77c69aa5a9baacc9b8fb6a1bf07cc8cab160d
mary ruthven <mary@mary-Latitude-E6420.(none)>
2014-12-15 11:14:08 -0500

    fix get road path

--- src/algorithms/dijkstra.py
+++ src/algorithms/dijkstra.py
@@ -135,10 +135,9 @@ class DijkstraSettlementAlgorithm(DijkstraAlgorithm):
         if path[-1] != self.source:
             raise ValueError('%s does not equal the expected start node, %s'
                              % (path[-1], self.source))
-        return self.get_road_path(path, source)
+        return self.get_road_path(source, path)
 
-    @staticmethod
-    def get_road_path(path, source):
+    def get_road_path(self, source, path):
         """Connect settlements in path using connecting vertices
         Returns:
             The list of connecting roads,
